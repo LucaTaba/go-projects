071b280a12f45307d4b486b50a503926af223645
LucaTaba <tabariniluca@email.com>
2025-03-14 23:16:38 +0100

    somma dato l'array da codice

--- Somma_array_interi/Somma_array_interi.go
+++ Somma_array_interi/Somma_array_interi.go
@@ -8,9 +8,15 @@ import (
 func main() {
 	start := time.Now()
 
+	var somma int = 0
+
 	numeri := [5]int{1, 2, 3, 4, 5}
 
-	fmt.Println(numeri[0])
+	for i := 0; i < len(numeri); i++ {
+		somma = somma + numeri[i]
+	}
+
+	fmt.Printf("La somma dell'array è %d \n", somma)
 
 	elapsed := time.Since(start)
 	fmt.Printf("Tempo di esecuzione: %s\n", elapsed)
