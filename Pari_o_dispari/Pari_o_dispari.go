a4ba12899e4e820758efd2226c59b21693f2abc4
LucaTaba <tabariniluca@email.com>
2025-03-14 22:35:13 +0100

    Aggiornamento con cattura numero

--- Pari_o_dispari/Pari_o_dispari.go
+++ Pari_o_dispari/Pari_o_dispari.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var nome string
-	var eta int
+	var numero int
 
-	n, err := fmt.Scanln(&nome, &eta)
+	n, err := fmt.Scanln(&numero)
 	fmt.Println("Ciao, mondo!")
 
 	if err != nil {
 		fmt.Println("Errore durante la lettura:", err)
 	}
-	fmt.Println("Errore durante la lettura:", n)
+	fmt.Println("Il tuo numero è:", numero)
+	fmt.Println("Elementi letti:", n)
 	return
 
 }
