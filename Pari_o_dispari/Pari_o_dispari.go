0820efc909c4080e9b0f403f88f6390bbcd421b4
LucaTaba <tabariniluca@email.com>
2025-03-14 23:01:06 +0100

    Sistemato codice per una megliore comprensione

--- Pari_o_dispari/Pari_o_dispari.go
+++ Pari_o_dispari/Pari_o_dispari.go
@@ -5,17 +5,18 @@ import "fmt"
 func main() {
 	var numero int
 
+	fmt.Println("Inserisci un numero:")
 	n, err := fmt.Scanln(&numero)
 
 	if err != nil {
 		fmt.Println("Errore durante la lettura:", err)
 	}
 	if numero%2 == 0 {
-		fmt.Println("Il tuo numero è pari")
+		fmt.Printf("Il numero %d è pari.\n", numero)
 	} else {
-		fmt.Println("Il tuo numero è dispari")
+		fmt.Printf("Il numero %d è dispari.\n", numero)
 	}
-	fmt.Println("Elementi letti:", n)
+	fmt.Printf("Elementi letti: %d\n", n)
 	return
 
 }
