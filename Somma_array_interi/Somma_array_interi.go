22e846dd7cd7d4c72b13cce5540aee0b8e087031
LucaTaba <tabariniluca@email.com>
2025-03-15 10:06:36 +0100

    Aggiornamento con input preso da terminale

--- Somma_array_interi/Somma_array_interi.go
+++ Somma_array_interi/Somma_array_interi.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
+
+	//gestione input
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	//inizio tempo
 	start := time.Now()
 
-	var somma int = 0
+	input := scanner.Text()
 
-	numeri := [5]int{1, 2, 3, 4, 5}
+	numeriStringhe := strings.Split(input, " ")
+
+	numeri := make([]int, len(numeriStringhe))
+
+	// Itera sulle sottostringhe e convertile in interi.
+	for i, numStr := range numeriStringhe {
+		// Usa strconv.Atoi per convertire la sottostringa in un intero.
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			// Gestisci l'errore se la conversione fallisce.
+			fmt.Println("Errore di conversione:", err)
+			return
+		}
+		// Assegna l'intero all'array.
+		numeri[i] = num
+	}
+
+	var somma int = 0
 
 	for i := 0; i < len(numeri); i++ {
 		somma = somma + numeri[i]
